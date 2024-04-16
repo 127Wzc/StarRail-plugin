a1446bda6b1b84a24320cdc0a31290f90b4036fa
127Wzc <52766844+127Wzc@users.noreply.github.com>
2024-04-16 10:23:28 +0800

    Update common.js

--- utils/common.js
+++ utils/common.js
@@ -2,7 +2,7 @@ import { exec} from 'child_process'
 import User from '../../genshin/model/user.js'
 import { getStoken} from './authkey.js'
 
-export const rulePrefix = '((#sr|\\*)?(sr))'
+export const rulePrefix = '((#|\\*)?(sr))'
 
 export async function checkPnpm () {
   let npm = 'npm'
